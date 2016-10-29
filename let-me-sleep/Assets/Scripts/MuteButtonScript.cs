97d613d6495304c3ca41173b79d66475cfb6afbc
Daniel <kaufi07@gmail.com>
2016-10-29 17:27:46 +0200

    menu scene with placeholders

---
+++ let-me-sleep/Assets/Scripts/MuteButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MuteButtonScript : MonoBehaviour {
+
+    private Button audioButton;
+    private bool soundMuted = false;
+	// Use this for initialization
+	void Start () {
+
+        audioButton = GameObject.FindGameObjectWithTag("MuteButton").GetComponent<Button>();
+        audioButton.onClick.AddListener(() => ToggleMuteSound());
+    }
+	
+
+    void ToggleMuteSound()
+    {
+        soundMuted = !soundMuted;
+        AudioListener.volume = soundMuted ? 0 : 1;
+        Debug.Log("sound is muted: " + soundMuted);
+    }
+}
