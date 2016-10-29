f6eb3d08bd5d0de9a0624e9067b6bd8cff5e9c9b
Daniel <kaufi07@gmail.com>
2016-10-29 17:50:09 +0200

    deleted mutebutton script. added menu script. added inital scenes for game states.

---
+++ let-me-sleep/Assets/Scripts/MenuController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    private UnityEngine.UI.Button audioButton;
+    private UnityEngine.UI.Button highScoreButton;
+    private UnityEngine.UI.Button startButton;
+    private UnityEngine.UI.Button howToButton;
+    private bool soundMuted = false;
+
+
+    // Use this for initialization
+    void Start () {
+        audioButton = GameObject.FindGameObjectWithTag("MuteButton").GetComponent<Button>();
+        highScoreButton = GameObject.FindGameObjectWithTag("Highscore").GetComponent<Button>();
+        startButton = GameObject.FindGameObjectWithTag("Game").GetComponent<Button>();
+        howToButton = GameObject.FindGameObjectWithTag("HowTo").GetComponent<Button>();
+        audioButton.onClick.AddListener(() => ToggleMuteSound());
+        highScoreButton.onClick.AddListener(() => loadScene("Highscore"));
+        startButton.onClick.AddListener(() => loadScene("Game"));
+        howToButton.onClick.AddListener(() => loadScene("HowTo"));
+    }
+
+
+    void loadScene(string scene)
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+    
+
+    void ToggleMuteSound()
+    {
+        soundMuted = !soundMuted;
+        AudioListener.volume = soundMuted ? 0 : 1;
+        Debug.Log("sound is muted: " + soundMuted);
+    }
+}
