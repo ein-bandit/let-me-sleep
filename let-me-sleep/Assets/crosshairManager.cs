a11741ac752dcd9c88cc0bf81f8f6e23cd55eda9
Leon <blackhawkacex@gmail.com>
2016-10-30 14:47:28 +0100

    Crosshair

---
+++ let-me-sleep/Assets/crosshairManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class crosshairManager : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        this.gameObject.transform.position = new Vector3(mousePos.x, mousePos.y, 1f);
+        print(this.gameObject.transform.position);
+    }
+}
