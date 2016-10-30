d6ae35adc71d2eae2da5b3d231c4d4767f411bbb
Daniel <kaufi07@gmail.com>
2016-10-30 15:15:40 +0100

    fixed crosshaird. fixed game screen name enter.

--- let-me-sleep/Assets/Scripts/TimeManager.cs
+++ let-me-sleep/Assets/Scripts/TimeManager.cs
@@ -4,12 +4,15 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 
-public class TimeManager : MonoBehaviour {
+public class TimeManager : MonoBehaviour
+{
 
     public Text uiText;
     public InputField playerName;
     public SaveHighscore saveHighscoreScript;
     public GameObject scoreCanvas;
+
+    public GameObject GameOver;
 
     //dummy init
     private int points_hits = 0;
@@ -21,9 +24,11 @@ public class TimeManager : MonoBehaviour {
     float remainingTime = 15.4f; // used to determine how long game lasts
     float bonusTime = 1.0f; // amount of time bonus you get on shot
 
-	// Use this for initialization
-	void Start () {
-        
+    // Use this for initialization
+    void Start()
+    {
+
+        GameOver.GetComponent<SpriteRenderer>().enabled = false;
         stopTime = false;
         showScore = false;
         Time.timeScale = 1.0f;
@@ -31,11 +36,12 @@ public class TimeManager : MonoBehaviour {
         //override points if not first time in scene;
         points_hits = 0;
         points_misses = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(!stopTime)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!stopTime)
         {
             remainingTime -= Time.deltaTime;
             timeLasted += Time.deltaTime;
@@ -43,7 +49,7 @@ public class TimeManager : MonoBehaviour {
             //uiText.text = remainingTime.ToString("#");
 
             // if time runs out -> show score canvas with input field for player
-            if(remainingTime <= 0.0f)
+            if (remainingTime <= 0.0f)
             {
                 // set canvas score variable to true
                 showScore = true;
@@ -51,23 +57,30 @@ public class TimeManager : MonoBehaviour {
                 // stop game times
                 Time.timeScale = 0f;
                 stopTime = true;
-                
+
             }
         }
         // show canvas for player name
         scoreCanvas.SetActive(showScore);
+
         // set focus on player input field
         EventSystem.current.SetSelectedGameObject(playerName.gameObject, null);
         playerName.OnPointerClick(new PointerEventData(EventSystem.current));
 
-        if (showScore && Input.GetKeyDown("return"))
+        if (showScore)
         {
-            //implement a crazy function using points_ vars and timelasted.
-            //int highscoreToSafe = (int) Mathf.Round(timeLasted);
-            saveHighscoreScript.saveHighScore(playerName.text,points_hits);
-            SceneManager.LoadScene("Highscore");
+
+            GameOver.GetComponent<SpriteRenderer>().enabled = true;
+
+            if (Input.GetKeyDown("return"))
+            {
+                //implement a crazy function using points_ vars and timelasted.
+                //int highscoreToSafe = (int) Mathf.Round(timeLasted);
+                saveHighscoreScript.saveHighScore(playerName.text, points_hits);
+                SceneManager.LoadScene("Highscore");
+            }
         }
-	}
+    }
 
 
     public void addClick(bool hit)
@@ -76,7 +89,8 @@ public class TimeManager : MonoBehaviour {
         {
             points_hits += 1;
             remainingTime += bonusTime;
-        } else
+        }
+        else
         {
             points_misses += 1;
         }
--- let-me-sleep/Assets/crosshairManager.cs
+++ let-me-sleep/Assets/crosshairManager.cs
@@ -15,5 +15,9 @@ public class crosshairManager : MonoBehaviour {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         this.gameObject.transform.position = new Vector3(mousePos.x, mousePos.y, 1f);
         print(this.gameObject.transform.position);
+        if (Time.timeScale == 0)
+        {
+            Cursor.visible = true;
+        }
     }
 }
